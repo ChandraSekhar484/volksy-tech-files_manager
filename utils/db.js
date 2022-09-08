54316324f12cede4516ae7cbaa13c7e1da8748fe
juanescorreap <juanestebancope@outlook.com>
2022-09-07 19:00:54 -0500

    tasks 0 and 1

---
+++ utils/db.js
@@ -0,0 +1,34 @@
+const { MongoClient } = require('mongodb');
+
+class DBClient {
+  constructor () {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const url = `mongodb://${host}:${port}/`;
+
+    MongoClient.connect(url, (error, client) => {
+      if (error) {
+        this.db = false;
+      } else {
+        this.db = client.db(database);
+        this.users = this.db.collection('users');
+        this.files = this.db.collection('files');
+      }
+    });
+  }
+
+  isAlive () {
+    return !!this.db;
+  }
+
+  async nbUsers () {
+    return this.users.countDocuments();
+  }
+
+  async nbFiles () {
+    return this.files.countDocuments();
+  }
+}
+const dbClient = new DBClient();
+module.exports = dbClient;
